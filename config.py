c95889752973941984d3ec11d3eff1955e3e1d70
Matthew Carmichael <sealblox@gmail.com>
2026-03-05 05:47:41 +0000

    Backup

--- config.py
+++ config.py
@@ -1,12 +1,70 @@
-import os
-import configparser
+import os, blessed, configparser
+
+term = blessed.Terminal()
 
 class RaceCreator:
+    def select(self, heading, items):
+        last_key = ""
+        selection_index = 0
+        while last_key != "KEY_ENTER":
+            print(term.home + term.clear, end="")
+            print(term.on_color_rgb(60,60,60) + term.center(heading) + term.normal)
+            for indx, item in enumerate(items):
+                clr = (indx == selection_index) and term.on_color_rgb(50,255,50) or term.on_color_rgb(60,60,60)
+                print(term.center(clr + item + term.normal))
+
+            val = term.inkey()
+
+            if val.is_sequence:
+                last_key = val.name
+            else:
+                last_key = val
+            
+            if last_key == "KEY_UP":
+                selection_index -= 1
+            elif last_key == "KEY_DOWN":
+                selection_index += 1
+            
+            if selection_index < 0:
+                selection_index = len(items)-1
+            elif selection_index > (len(items)-1):
+                selection_index = 0
+        
+        return items[selection_index]
+
+    def centered_input(self, prompt):
+        last_key = ""
+        txt = ""
+        while last_key != "KEY_ENTER":
+            print(term.home + term.clear, end="")
+            print(term.on_color_rgb(60,60,60) + term.center(prompt) + term.normal)
+            print(term.center("> " + txt))
+
+            val = term.inkey()
+
+            if val.is_sequence:
+                last_key = val.name
+
+                if val.name == "KEY_BACKSPACE":
+                    txt = txt[:-1]
+            else:
+                last_key = val
+                txt += last_key
+        
+        return txt
+
     def __init__(self):
-        print("-> Create Race File <-")
-        self.race_name = input("Race Name > ")
-        self.car_file = input("Car File > ")
-        self.track_file = input("Track File > ")
+        print(term.home + term.normal + term.clear, end="")
+        print(term.on_color_rgb(60,60,60) + term.center("Create Race File") + term.normal)
+        print(term.move_xy(0, round(term.height/2)) + term.center("Press any key to begin."))
+
+        with term.cbreak():
+            term.inkey()
+
+            self.race_name = self.centered_input("Enter Race Name")
+
+            self.car_file = "cars/" + self.select("Select Car File", os.listdir("cars"))
+            self.track_file = "tracks/" + self.select("Select Track File", os.listdir("tracks"))
 
         self.car_config = configparser.ConfigParser()
         self.car_config.read(self.car_file)
@@ -68,6 +126,4 @@ class RaceCreator:
             self.race_config.write(file)
 
 if __name__ == "__main__":
-    action = input("[c]reate : [m]odify $ ")
-    if action == "c":
-        race_creator = RaceCreator()
+    race_creator = RaceCreator()
